53d9ababa3cdbb5de65dfd383a39d473fe0bac6d
Panindra Chekoori <pxchek.developer@gmail.com>
2022-09-23 21:53:45 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.concurrent.Callable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws SocketException {
+        System.out.println( "Hello World!" );
+    }
+}
