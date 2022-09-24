1723dc67ed45de93802f037ea5f30cc1325cd10b
Panindra Chekoori <pxchek.developer@gmail.com>
2022-09-23 22:04:43 -0500

    Update the source and pom files.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,16 +1,11 @@
 package com.mycompany.app;
-
-import java.net.Socket;
-import java.net.SocketException;
-import java.util.concurrent.Callable;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ) throws SocketException {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
